ac8fa9dccdda95eecd8b46d7df422eb82c976588
Kenz-Developer <kenzorkut49@gmail.com>
2025-03-02 06:07:03 +0700

    Update github.ts

--- src/utils/github.ts
+++ src/utils/github.ts
@@ -6,8 +6,8 @@ const octokit = new Octokit({
   auth: import.meta.env.VITE_GITHUB_TOKEN,
 });
 
-const owner = 'NamaGithub';
-const repo = 'NamaRepo';
+const owner = 'kenz-developer';
+const repo = 'Base-Web-SCode';
 
 export async function getUsers(): Promise<User[]> {
   try {
